4d301a8fdd4b6f3a9ba47a6d07f325bd76083d27
innocentemutabazi <innocentemutabazi@gmail.com>
2024-06-07 13:02:28 +0200

    Added more comments and formatted the code

--- gallery.js
+++ gallery.js
@@ -1,28 +1,25 @@
+/*I decided to use event delegation for the sake of performance 
+whereby I will be adding the event on the body (the gallery-1 class)
+ instead of adding it to the animal cards*/
 document.addEventListener('DOMContentLoaded', () => {
     // Selecting the gallery container element with the class 'gallery-1'
     const gallery = document.querySelector('.gallery-1');
     // Add an event listener to the gallery for the 'mouseover' event
-
     gallery.addEventListener('mouseover', ({ target }) => {
         // Find the nearest parent element with the class 'image-interaction'
         // and then select its child element with the class 'overlay'
-
         const overlay = target.closest('.image-interaction')?.querySelector('.overlay');
         // If the overlay exists and does not already contain an element with the class 'action-button'
-
         if (overlay && !overlay.querySelector('.action-button')) {
             // Append the "Know More" link inside the overlay
-
             overlay.innerHTML += '<div class="action-button"><p>Know More &rarr;</p></div>';
         }
     });
     // Add an event listener to the gallery for the 'mouseout' event
-
     gallery.addEventListener('mouseout', ({ target }) => {
         // Find the nearest parent element with the class 'image-interaction'
         // and then select its child element with the class 'action-button'
         // If the action button exists, remove it from the DOM
-
         target.closest('.image-interaction')?.querySelector('.action-button')?.remove();
     });
 });
