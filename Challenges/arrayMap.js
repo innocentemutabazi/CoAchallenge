68b0d1cdf14031603b8878d85db60f303700c23a
innocentemutabazi <innocentemutabazi@gmail.com>
2024-06-08 01:20:54 +0200

    Fixed a small bug on the arraymap.js

--- Challenges/arrayMap.js
+++ Challenges/arrayMap.js
@@ -1,12 +1,12 @@
 const arrayMap = (arr, target) => {
   let left=0 ,currentSum = 0
     for (let right = 0; right < arr.length; right++) {
+        if(arr[right]>0 && target === 0) return false
         currentSum += arr[right]
         while (currentSum > target) {
             currentSum -= arr[left]; left++
         }
-        if (currentSum === target)return true
+        if (currentSum === target )return true
     }
     return false
 }
-console.log(arrayMap([4, 2, 7, 1, 9, 5], 17)) 
