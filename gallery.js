1dec283480ca4cfc10869b909bc6cb372e558e3c
innocentemutabazi <innocentemutabazi@gmail.com>
2024-06-07 12:51:22 +0200

    refactor: Simplified and optimized hover effect in the JS code

--- gallery.js
+++ gallery.js
@@ -1,19 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.gallery-1').addEventListener('mouseover', handleMouse);
-    document.querySelector('.gallery-1').addEventListener('mouseout', handleMouse);
-
-    function handleMouse(event) {
-        const imageInteraction = event.target.closest('.image-interaction');
-        if (!imageInteraction) return;
-        const overlay = imageInteraction.querySelector('.overlay');
-        if (!overlay) return;
-
-        if (event.type === 'mouseover' && !overlay.querySelector('.action-button')) {
-            overlay.insertAdjacentHTML('beforeend', '<div class="action-button"><p>Know More &rarr;</p></div>');
-        } else if (event.type === 'mouseout' && overlay.querySelector('.action-button')) {
-            const link = overlay.querySelector('.action-button');
-            link.remove();
+    const gallery = document.querySelector('.gallery-1');
+    gallery.addEventListener('mouseover', ({ target }) => {
+        const overlay = target.closest('.image-interaction')?.querySelector('.overlay');
+        if (overlay && !overlay.querySelector('.action-button')) {
+            overlay.innerHTML += '<div class="action-button"><p>Know More &rarr;</p></div>';
         }
-    }
-
+    });
+    gallery.addEventListener('mouseout', ({ target }) => {
+        target.closest('.image-interaction')?.querySelector('.action-button')?.remove();
+    });
 });
+
