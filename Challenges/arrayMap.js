b65250c63f75fb94106a07466e79ebf5d515890d
innocentemutabazi <innocentemutabazi@gmail.com>
2024-06-08 15:37:01 +0200

    Renamed the function to a more descriptive name

--- Challenges/arrayMap.js
+++ Challenges/arrayMap.js
@@ -1,12 +1,16 @@
-const arrayMap = (arr, target) => {
-  let left=0 ,currentSum = 0
+const hasSubarrayWithTargetSum = (arr, target) => {
+    let left = 0;
+    let currentSum = 0;
     for (let right = 0; right < arr.length; right++) {
-        if(arr[right]>0 && target === 0) return false
-        currentSum += arr[right]
-        while (currentSum > target) {
-            currentSum -= arr[left]; left++
+        if (arr[right] > 0 && target === 0) return false;
+        currentSum += arr[right];
+        while (currentSum > target && left <= right) {
+            currentSum -= arr[left];
+            left++;
         }
-        if (currentSum === target )return true
+        if (currentSum === target) return true;
     }
-    return false
-}
+    return false;
+};
+
+console.log(hasSubarrayWithTargetSum([1, 2, 3, 4, 5, 6, 34], 0));
