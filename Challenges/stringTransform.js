b30ef0704f1ab79d16945340141747324eda5751
Innocente Umuhuza Mutabazi <159420918+innocentemutabazi@users.noreply.github.com>
2024-06-09 02:36:23 +0200

    Update stringTransform.js

--- Challenges/stringTransform.js
+++ Challenges/stringTransform.js
@@ -1,11 +1,11 @@
-const stringTransform = inputString => {
-    let characters = [...(inputString.replaceAll(' ', ''))];
-    if (inputString.length % 15 === 0) { 
+const stringTransform = str => {
+    let characters = [...(str.replaceAll(' ', ''))];
+    if (str.length % 15 === 0) { 
       return characters.reverse().map(char => char.charCodeAt(0).toString()).join(' ');
-    } else if (inputString.length % 3 === 0) { 
+    } else if (str.length % 3 === 0) { 
       return characters.reverse().join('');
-    } else if (inputString.length % 5 === 0) { 
+    } else if (str.length % 5 === 0) { 
       return characters.map(char => char.charCodeAt(0).toString()).join(' ');
     }
-    return inputString;
+    return str;
 };
